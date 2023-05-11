83aa4a735a0ba3aac2adb97bf5dc89d3d7453695
Ozkan Pakdil <ozkan.pakdil@gmail.com>
2023-05-11 06:44:18 +0100

    * menu bar added, not very functional

--- src/main/java/com/mascix/swaggerific/MainController.java
+++ src/main/java/com/mascix/swaggerific/MainController.java
@@ -2,17 +2,24 @@ package com.mascix.swaggerific;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Window;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    @FXML
+    VBox mainBox;
 
     ObjectMapper mapper = new ObjectMapper();
     @FXML
@@ -21,7 +28,14 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        /*Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        /*
+        showAlert();*/
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    }
+
+    public void showAlert() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Message Here...");
         alert.setHeaderText("Look, an Information Dialog");
         alert.setContentText("I have a great message for you!");
@@ -30,13 +44,39 @@ public class MainController implements Initializable {
                 System.out.println("Pressed OK.");
             }
         });
-        showAlert();*/
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        String webApiUrl = "https://petstore.swagger.io/v2/swagger.json";
+    }
+
+    public void handleKeyInput(KeyEvent keyEvent) {
+
+    }
+
+    public void handleAboutAction(ActionEvent actionEvent) {
+        showAlert();
+    }
+
+    public void menuFileExit(ActionEvent actionEvent) {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public void menuFileOpenSwagger(ActionEvent actionEvent) {
+        TextInputDialog dialog = new TextInputDialog("https://petstore.swagger.io/v2/swagger.json");
+        dialog.setTitle("Enter swagger url");
+        dialog.setContentText("URL:");
+        dialog.setHeaderText("Enter the json url of swagger url.");
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(string -> {
+            mainBox.setDisable(true);
+            openSwaggerUrl(string);
+            mainBox.setDisable(false);
+        });
+    }
+    @DisableWindow
+    private void openSwaggerUrl(String urlSwagger) {
+        String webApiUrl = urlSwagger;
         TreeItem<String> root = new TreeItem<>("base root");
         treePaths.setRoot(root);
         try {
-
             SwaggerModal jsonNode = mapper.readValue(new URL(webApiUrl), SwaggerModal.class);
             jsonNode.getTags().forEach(it -> {
                 TreeItem<String> tag = new TreeItem<>();
@@ -55,17 +95,5 @@ public class MainController implements Initializable {
             throw new RuntimeException(e);
         }
         treePaths.setShowRoot(false);
-    }
-
-    public void showAlert() {
-        Platform.runLater(new Runnable() {
-            public void run() {
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Message Here...");
-                alert.setHeaderText("Look, an Information Dialog");
-                alert.setContentText("I have a great message for you!");
-                alert.showAndWait();
-            }
-        });
     }
 }
