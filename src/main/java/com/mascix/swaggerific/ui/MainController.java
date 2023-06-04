5c224cf1fa21f581dfccf7ad7fcf6c2176b565c5
Ozkan Pakdil <ozkan.pakdil@gmail.com>
2023-06-04 23:44:05 +0100

    switch to "if else"

--- src/main/java/com/mascix/swaggerific/ui/MainController.java
+++ src/main/java/com/mascix/swaggerific/ui/MainController.java
@@ -261,15 +261,13 @@ public class MainController implements Initializable {
     public void btnSendRequest(ActionEvent actionEvent) {
         TreeItem<String> selectedItem = (TreeItem<String>) treePaths.getSelectionModel().getSelectedItem();
 
-        switch (selectedItem.getValue()) {
-            case "GET":
-                Platform.runLater(() -> getRequest());
-            case "POST":
-                Platform.runLater(() -> postRequest());
-            default:
-                log.error(selectedItem.getValue() + " not implemented yet");
+        if (selectedItem.getValue().equals("GET")) {
+            Platform.runLater(() -> getRequest());
+        } else if (selectedItem.getValue().equals("POST")) {
+            Platform.runLater(() -> postRequest());
+        } else {
+            log.error(selectedItem.getValue() + " not implemented yet");
         }
-
     }
 
     @SneakyThrows
