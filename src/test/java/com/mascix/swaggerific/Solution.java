942f4c90ec35725f5f13a3c9d601aae33cfd414e
ozkanpakdil <ozkan.pakdil@gmail.com>
2023-09-22 19:07:32 +0100

    just test code

--- src/test/java/com/mascix/swaggerific/Solution.java
+++ src/test/java/com/mascix/swaggerific/Solution.java
@@ -1,64 +1,31 @@
 package com.mascix.swaggerific;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
-
-class Result {
-
-    /*
-     * Complete the 'jimOrders' function below.
-     *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts 2D_INTEGER_ARRAY orders as parameter.
-     */
-
-    public static List<Integer> jimOrders(List<List<Integer>> orders) {
-        HashMap<Integer, Integer> map = new HashMap();
-        int i = 0;
-        for (List<Integer> el : orders) {
-            map.put(++i, el.get(0) + el.get(1));
-        }
-
-        return map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey)
-                .collect(toList());
-    }
-
-}
+import java.util.Arrays;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[]{1, 3, 5, 7, 9};
+        int K = 2;
+        System.out.println(Arrays.toString(turnTheArray(arr, K)));
 
-        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        List<List<Integer>> orders = new ArrayList<>();
 
-        IntStream.range(0, n).forEach(i -> {
-            try {
-                orders.add(
-                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                                .map(Integer::parseInt)
-                                .collect(toList())
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
+
+
+    }
+
+    private static int[] turnTheArray(int[] nums, int k) {
+        for (int j=0; j<k; j++)
+        {
+            for (int i=nums.length-1; 0<i; i--)
+            {
+                // shifting towards the right
+                int temp = nums[i - 1];
+                nums[i-1]=nums[i];
+                nums[i]=temp;
             }
-        });
-
-        List<Integer> result = Result.jimOrders(orders);
-        System.out.println(result);
-
-        bufferedReader.close();
+        }
+        return nums;
     }
 }
