46ccdaff0e37fcdc57dd44380074d3099cbffbed
Özkan Pakdil <ozkan.pakdil@gmail.com>
2023-10-08 22:02:03 +0100

    solving some algorithm problems

--- src/test/java/com/mascix/swaggerific/algorithms/RotateArray.java
+++ src/test/java/com/mascix/swaggerific/algorithms/RotateArray.java
@@ -1,4 +1,4 @@
-package com.mascix.swaggerific;
+package com.mascix.swaggerific.algorithms;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Solution {
+public class RotateArray {
     public static void main(String[] args) throws IOException {
         int[] arr = new int[]{1, 3, 5, 7, 9};
         int K = 3;
@@ -17,7 +17,7 @@ public class Solution {
         System.out.println("-----------");
         // no need to reinvent the wheel.
         List l = new ArrayList<>();
-        Arrays.stream(arr).forEach(i->l.add(i));
+        Arrays.stream(arr).forEach(i -> l.add(i));
         Collections.rotate(l, K); // rotate right
         System.out.println(Arrays.toString(l.toArray()));
         Collections.rotate(l, -1 * K); // rotate left
@@ -26,7 +26,7 @@ public class Solution {
 
     private static int[] turnTheArrayToRight(int[] nums, int k) {
         for (int j = 0; j < k; j++) {
-            for (int i = nums.length - 1; 0 < i; i--) {
+            for (int i = nums.length - 1; i > 0; i--) {
                 int temp = nums[i - 1];
                 nums[i - 1] = nums[i];
                 nums[i] = temp;
